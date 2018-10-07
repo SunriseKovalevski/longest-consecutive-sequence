dc0927d78649df3042d91c8686923ce5862fef74
SunriseKovalevski <Sunrisekovalevsky283@gmail.com>
2018-10-07 23:29:13 +0300

    finally

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+   // your solution here
+
+  let set = new Set();
+  if(!array.length)return 0;
+  let result = 1;
+  for(let a of array){
+    set.add(a);
+  }
+  for(let a of array){
+    let c = 1;
+    let l = a-1,r=a+1;
+    while(set.has(l)){
+      ++c;
+      set.delete(l);
+      --l;
+    }
+    while(set.has(r)){
+      ++c;
+      set.delete(r);
+      ++r;
+    }
+    result = Math.max(c,result);
+  }
+  return result;
 }
